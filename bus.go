bcf7e91f7a0fa17ae7549e4a3d4584d8261bc15e
Allan Garcia <a.g.jesrez@gmail.com>
2023-01-25 23:43:54 -0600

    created write

--- bus.go
+++ bus.go
@@ -6,6 +6,7 @@ type Bus struct {
 	// Address contains the information of the address bus.
 	Address uint16
 
+	// FIXME: abstract these into proper definitions outside the Bus.
 	CPU_RAM   []uint8
 	CPU_Stack []uint8
 
@@ -35,6 +36,36 @@ func (b *Bus) Read() {
 		// Expansion ROM
 	case addr < 0x8000: // SRAM
 		b.Data = b.Cartridge.PrgRAM[addr]
+	case addr < 0xC000: // PRG-ROM lower bank
+		b.Data = b.Mapper.ReadLowerBank(addr)
+	default: // PRG-ROM upper bank (addr < 0x10000)
+		b.Data = b.Mapper.ReadUpperBank(addr)
+	}
+}
+
+func (b *Bus) Write() {
+	addr := b.Address
+	data := b.Data
+
+	switch {
+	case addr < 0x0100:
+		// Zero Page
+	case addr < 0x0200: // Stack
+		b.CPU_Stack[addr] = data
+	case addr < 0x0800: // CPU RAM
+		b.Data = b.CPU_RAM[addr]
+	case addr < 0x2000:
+		// Mirrors ($0000-$07FF)
+	case addr < 0x2008:
+		// I/O Registers
+	case addr < 0x4000:
+		// Mirrors ($2000-$2007)
+	case addr < 0x4020:
+		// I/O Registers
+	case addr < 0x6000:
+		// Expansion ROM
+	case addr < 0x8000: // SRAM
+		b.Cartridge.PrgRAM[addr] = data
 	case addr < 0xC000: // PRG-ROM lower bank
 		b.Data = b.Mapper.ReadLowerBank(addr)
 	default: // PRG-ROM upper bank (addr < 0x10000)
