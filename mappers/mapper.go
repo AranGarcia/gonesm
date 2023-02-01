6046d3c351c45b2f26b784265b43d6be59df1924
Allan Garcia <a.g.jesrez@gmail.com>
2023-02-01 00:49:07 -0600

    nrom mapper implementation

--- mappers/mapper.go
+++ mappers/mapper.go
@@ -3,7 +3,7 @@ package mappers
 type MapperType uint
 
 const (
-	UNROM MapperType = iota
+	NROM MapperType = iota
 )
 
 // MMC is the memory management controller interface. Dependending on the implementation,
@@ -16,3 +16,37 @@ type MMC interface {
 	// WritePRG saves data in the address of the PRG memory.
 	WritePRG(uint16, uint8)
 }
+
+type Specs struct {
+	// PRGBanks specifies the amount of PRG banks.
+	PRGBanks uint
+	// CHRBanks specifies the amount of CHR banks.
+	CHRBanks uint
+}
+
+// NewMapper returns a memory management controller implementation according to the specified
+// mapper number.
+func NewMapper(mt MapperType, s Specs) MMC {
+	var mmc MMC
+	switch mt {
+	case NROM:
+		mmc = &nromMapper{
+			prg: make([]byte, 0x4000*s.PRGBanks),
+			chr: make([]byte, 0x2000*s.CHRBanks),
+		}
+	}
+
+	return mmc
+}
+
+// nromMapper is MMC implementation for the NROM (no mapper).
+type nromMapper struct {
+	prg []byte
+	chr []byte
+}
+
+func (m *nromMapper) ReadCHR(addr uint16) uint8 { return m.chr[addr] }
+
+func (m *nromMapper) ReadPRG(addr uint16) uint8 { return m.prg[addr] }
+
+func (m *nromMapper) WritePRG(addr uint16, data uint8) { m.prg[addr] = data }
