afa705c0d54fb78b56c3c9ccdd607c6ea5eb9b09
Allan Garcia <a.g.jesrez@gmail.com>
2023-02-01 00:22:14 -0600

    refactored interface

--- mappers/mapper.go
+++ mappers/mapper.go
@@ -4,12 +4,15 @@ type MapperType uint
 
 const (
 	UNROM MapperType = iota
-	CNROM
-	MMC1
-	MMC3
 )
 
-type Mapper interface {
-	Read(uint16) uint8
-	Write(uint16, uint8)
+// MMC is the memory management controller interface. Dependending on the implementation,
+// read/write operations may or may not include memory bank switching.
+type MMC interface {
+	// ReadCHR returns data located in the address of CHR memory.
+	ReadCHR(uint16) uint8
+	// ReadPRG returns data located in the address of PRG memory.
+	ReadPRG(uint16) uint8
+	// WritePRG saves data in the address of the PRG memory.
+	WritePRG(uint16, uint8)
 }
